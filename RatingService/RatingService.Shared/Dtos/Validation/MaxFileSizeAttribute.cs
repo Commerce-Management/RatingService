d71f381be8291a12f45d7d80730c2e9f728b182f
Ziya Hacili <magahajili@gmail.com>
2025-12-26 23:55:30 +0400

    image val for dto

---
+++ RatingService/RatingService.Shared/Dtos/Validation/MaxFileSizeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+
+namespace RatingService.Shared.Dtos.Validation;
+
+public class MaxFileSizeAttribute(int maxFileSize) : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is IFormFile file)
+        {
+            if (file.Length > maxFileSize)
+                return new ValidationResult($"File size cannot exceed {maxFileSize} bytes.");
+        }
+        return ValidationResult.Success;
+    }
+}
