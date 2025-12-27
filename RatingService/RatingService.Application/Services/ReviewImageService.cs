5d9eb86d4cf6bd9eaca28bf57c1ec660b57d67a6
Ziya Hacili <magahajili@gmail.com>
2025-12-27 23:53:43 +0400

    add services for image and review

---
+++ RatingService/RatingService.Application/Services/ReviewImageService.cs
@@ -0,0 +1,79 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using RatingService.Core.Interfaces;
+﻿using Imagekit.Sdk;
+using RatingService.Shared.Dtos;
+
+namespace RatingService.Application.Services;
+
+public class ReviewImageService(IConfiguration configuration) : IReviewImageService
+{
+    private readonly ImagekitClient _imagekitClient = new (configuration["ImageKit:PublicKey"], configuration["ImageKit:PrivateKey"], configuration["ImageKit:UrlEndPoint"]);
+    private readonly string _privateKey = configuration["ImageKit:PrivateKey"]!;
+
+    public async Task<string> UploadImageAsync(IFormFile imageData)
+    {
+        if (imageData == null || imageData.Length == 0)
+            throw new ArgumentException("Image data is null or empty.", nameof(imageData));
+
+        using var memoryStream = new MemoryStream();
+        await imageData.CopyToAsync(memoryStream);
+        var fileBytes = memoryStream.ToArray();
+
+        var ob = new FileCreateRequest
+        {
+            file = fileBytes,
+            fileName = Guid.NewGuid().ToString(),
+            folder = "/reviews"
+        };
+        
+        var result = await _imagekitClient.UploadAsync(ob);
+        return result.url;
+    }
+    
+
+    public async Task<bool> DeleteImageAsync(string imageUrl)
+    {
+        var uri = new Uri(imageUrl);
+
+        // имя файла без папки
+        var fileName = Path.GetFileName(uri.AbsolutePath);
+
+        var auth = Convert.ToBase64String(
+            Encoding.UTF8.GetBytes($"{_privateKey}:"));
+
+        using var client = new HttpClient();
+        client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Basic", auth);
+
+        var searchResponse = await client.PostAsJsonAsync(
+            "https://api.imagekit.io/v1/files/search",
+            new
+            {
+                searchQuery = $"name = \"{fileName}\""
+            });
+
+        if (!searchResponse.IsSuccessStatusCode)
+            return false;
+
+        var files = await searchResponse.Content
+            .ReadFromJsonAsync<List<ImageKitSearchResult>>();
+
+        var file = files?.SingleOrDefault();
+        if (file == null)
+            return false;
+
+        var deleteResponse = await client.DeleteAsync(
+            $"https://api.imagekit.io/v1/files/{file.fileId}");
+
+        return deleteResponse.IsSuccessStatusCode;
+    }
+
+
+
+
+    
+}
---
+++ RatingService/RatingService.Application/Services/ReviewService.cs
@@ -0,0 +1,165 @@
+﻿using AutoMapper;
+using RatingService.Core.Entities;
+using RatingService.Core.Interfaces;
+using RatingService.Infrastructure.Interfaces.Base;
+using RatingService.Infrastructure.Interfaces.Entities;
+using RatingService.Shared.Dtos;
+
+namespace RatingService.Application.Services;
+
+public class ReviewService(
+    IUnitOfWork unitOfWork,
+    IProductReviewRepository productReviewRepository,
+    IReviewImageService imageService,
+    IMapper mapper
+    ) : IReviewService
+{
+    public async Task<ProductReview> CreateProductReview(CreateReviewDto reviewDto)
+    {
+        try
+        {
+            var review = mapper.Map<ProductReview>(reviewDto);
+
+            if (reviewDto.Images is { Length: > 0})
+            {
+                var imageUrls = new List<string>();
+                foreach (var image in reviewDto.Images)
+                {
+                    var url = await imageService.UploadImageAsync(image);
+                    imageUrls.Add(url);
+                }
+                review.ImageUrls = imageUrls.ToArray();
+            }
+            else
+                review.ImageUrls = Array.Empty<string>();
+
+            await unitOfWork.BeginTransactionAsync();
+
+            var insertedReview = await productReviewRepository.InsertAsync(review);
+            
+            await unitOfWork.SaveChangesAsync();
+            await unitOfWork.CommitTransactionAsync();
+
+
+            return insertedReview;
+        }
+        catch (Exception)
+        {
+            await unitOfWork.RollbackTransactionAsync();
+            throw;
+        }
+    }
+
+    public async Task<bool> UpdateProductReview(UpdateReviewDto reviewDto)
+    {
+        var review = await productReviewRepository.GetReviewByIdAsync(reviewDto.ReviewId);
+        if (review == null)
+            throw new KeyNotFoundException($"Review {reviewDto.ReviewId} not found!");
+
+        var oldImageUrls = review.ImageUrls?.ToList() ?? new List<string>();
+
+        if (reviewDto.Rating.HasValue)
+            review.Rating = reviewDto.Rating.Value;
+
+        if (reviewDto.IsAnonymous.HasValue)
+            review.IsAnonymous = reviewDto.IsAnonymous.Value;
+
+        if (reviewDto.Title != null)
+            review.Title = reviewDto.Title;
+
+        if (reviewDto.Text != null)
+            review.Text = reviewDto.Text;
+
+        var existingImageUrls = review.ImageUrls != null
+            ? review.ImageUrls.ToList()
+            : new List<string>();
+
+        if (reviewDto.RemoveImageUrls != null && reviewDto.RemoveImageUrls.Any())
+        {
+            existingImageUrls = existingImageUrls
+                .Where(url => !reviewDto.RemoveImageUrls.Contains(url))
+                .ToList();
+        }
+
+        if (reviewDto.NewImages != null && reviewDto.NewImages.Length > 0)
+        {
+            foreach (var image in reviewDto.NewImages)
+            {
+                var url = await imageService.UploadImageAsync(image);
+                existingImageUrls.Add(url);
+            }
+        }
+
+        var imagesToDelete = oldImageUrls
+            .Except(existingImageUrls)
+            .ToList();
+
+        foreach (var imageUrl in imagesToDelete)
+        {
+            await imageService.DeleteImageAsync(imageUrl);
+        }
+
+        review.ImageUrls = existingImageUrls.ToArray();
+        review.UpdatedAt = DateTime.UtcNow;
+
+        productReviewRepository.Update(review);
+        var result = await productReviewRepository.SaveChangesAsync();
+
+        return result > 0;
+    }
+
+
+    public async Task<bool> DeleteProductReview(Guid reviewId)
+    {
+        var review = await productReviewRepository.GetReviewByIdAsync(reviewId);
+        if (review == null)
+            throw new KeyNotFoundException($"Review {reviewId} not found!");
+
+        if (review.ImageUrls != null)
+        {
+            foreach (var imageUrl in review.ImageUrls)
+            {
+                await imageService.DeleteImageAsync(imageUrl);
+            }
+        }
+
+        productReviewRepository.Delete(review);
+        
+        var result = await productReviewRepository.SaveChangesAsync();
+        return result > 0;
+    }
+
+    public async Task<IEnumerable<ProductReview>> GetReviewsByProductId(Guid productId, int pageNumber, int pageSize)
+    {
+        var reviews = await productReviewRepository.GetAllProductReviewsByIdAsync(productId, pageNumber, pageSize);
+        return reviews;
+    }
+
+    public async Task<IEnumerable<ProductReview>> GetProductReviewsByRating(Guid productId, int rating, int page, int pageSize)
+    {
+        var reviews = await productReviewRepository.GetProductReviewsByRatingAsync(productId, rating, page, pageSize);
+        return reviews;
+    }
+
+    public async Task<IEnumerable<ProductReview>> GetReviewsByDate(DateTime date, int pageNumber, int pageSize)
+    {
+        var reviews = await productReviewRepository.GetReviewsByDateAsync(date, pageNumber, pageSize);
+        return reviews;
+    }
+
+    public async Task<IEnumerable<ProductReview>> GetAllReviews(int pageNumber, int pageSize)
+    {
+        var reviews = await productReviewRepository.GetAllReviewsAsync(pageNumber, pageSize: 30);
+        return reviews;
+    }
+
+    public async Task<ProductReview> GetReviewByid(Guid reviewId)
+    {
+        var review = await productReviewRepository.GetReviewByIdAsync(reviewId);
+
+        if (review == null)
+            throw new KeyNotFoundException($"Review {reviewId} not found!");
+
+        return review;
+    }
+}
