497eaa67e5246001bd63f43968c65bc2c0cc97ab
Ziya Hacili <magahajili@gmail.com>
2025-12-29 18:21:24 +0400

    add profile for dto

---
+++ RatingService/RatingService.Core/Profiles/QuestionsAndAnswersProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using RatingService.Core.Entities;
+using RatingService.Shared.Dtos;
+
+namespace RatingService.Core.Profiles;
+
+public class QuestionsAndAnswersProfile : Profile
+{
+    public QuestionsAndAnswersProfile()
+    {
+        CreateMap<CreateQuestionDto, ProductQuestion>()
+            .ForMember(dest => dest.Answers, opt => opt.Ignore())
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid()))
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow));
+        CreateMap<CreateAnswerDto, ProductAnswer>();
+        CreateMap<UpdateQuestionDto, ProductQuestion>();
+        CreateMap<UpdateAnswerDto, ProductAnswer>();
+    }
+}
