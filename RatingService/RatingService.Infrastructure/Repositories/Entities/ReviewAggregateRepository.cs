7cb4f716a2300323a9cc8f822d27e2ec2183e31e
Ziya Hacili <magahajili@gmail.com>
2026-01-09 23:51:31 +0400

    add aggregate repo

--- RatingService/RatingService.Infrastructure/Repositories/Entities/ReviewAggregateRepository.cs
+++ RatingService/RatingService.Infrastructure/Repositories/Entities/ReviewAggregateRepository.cs
@@ -6,59 +6,22 @@ using RatingService.Infrastructure.Repositories.Base;
 
 namespace RatingService.Infrastructure.Repositories.Entities;
 
-public class ReviewAggregateRepository(RatingDbContext context) : Repository<ProductReviewAggregate>(context), IReviewAggregateRepository
+public class ReviewAggregateRepository(RatingDbContext context, IProductReviewRepository productReviewRepository) : IReviewAggregateRepository
 {
-    private const int BayesianMinimumReviews  = 5;
 
     public async Task<ProductReviewAggregate?> GetReviewAggregateByProductIdAsync(Guid productId)
     {
-        return await Entities.AsNoTracking().SingleOrDefaultAsync(agr => agr.ProductId == productId);
+        return await context.ProductReviewAggregate
+            .SingleOrDefaultAsync(x => x.ProductId == productId);
     }
 
-    public async Task<ProductReviewAggregate?> RecalculateAggregateByProductIdAsync(Guid productId)
+    public async Task InsertAsync(ProductReviewAggregate aggregate)
     {
-        var reviews = await productReviewRepository
-            .GetAll()
-            .Where(r => r.ProductId == productId)
-            .ToListAsync();
+        await context.ProductReviewAggregate.AddAsync(aggregate);
+    }
 
-        var reviewCount = reviews.Count;
-
-        var averageRating = reviewCount == 0
-            ? 0
-            : reviews.Average(r => r.Rating);
-
-        // Global average по всей системе
-        var globalAverage = await productReviewRepository
-            .GetAll()
-            .AverageAsync(r => (double?)r.Rating) ?? 0;
-
-        var bayesianRating = reviewCount == 0
-            ? globalAverage
-            : (reviewCount * averageRating + BayesianMinimumReviews * globalAverage)
-              / (reviewCount + BayesianMinimumReviews);
-
-        var aggregate = await aggregateRepository.GetByProductIdAsync(productId);
-
-        if (aggregate == null)
-        {
-            aggregate = new ProductReviewAggregate
-            {
-                ProductId = productId,
-                ReviewCount = reviewCount,
-                AverageRating = averageRating,
-                BayesianRating = bayesianRating
-            };
-
-            await aggregateRepository.InsertAsync(aggregate);
-        }
-        else
-        {
-            aggregate.ReviewCount = reviewCount;
-            aggregate.AverageRating = averageRating;
-            aggregate.BayesianRating = bayesianRating;
-
-            aggregateRepository.Update(aggregate);
-        }
+    public void UpdateAsync(ProductReviewAggregate aggregate)
+    {
+        context.ProductReviewAggregate.Update(aggregate);
     }
 }
