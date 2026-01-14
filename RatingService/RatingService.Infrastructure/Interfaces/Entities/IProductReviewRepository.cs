a9ca12e63ea2d1bdede1df51ce96757e3fb05a00
Ziya Hacili <magahajili@gmail.com>
2026-01-14 23:59:20 +0400

    interface add

--- RatingService/RatingService.Infrastructure/Interfaces/Entities/IProductReviewRepository.cs
+++ RatingService/RatingService.Infrastructure/Interfaces/Entities/IProductReviewRepository.cs
@@ -9,5 +9,8 @@ public interface IProductReviewRepository : IRepository<ProductReview>
     public Task<IEnumerable<ProductReview>> GetAllReviewsAsync(int page, int pageSize);
     public Task<ProductReview?> GetReviewByIdAsync(Guid reviewId);
     public Task<IEnumerable<ProductReview>> GetProductReviewsByRatingAsync(Guid productId, int rating, int page, int pageSize);
+    public Task<IEnumerable<ProductReview>> GetReviewsByUserIdAsync(Guid userId, int page, int pageSize);
+    public Task<IEnumerable<ProductReview>> GetReviewsByUserIdAndRatingAsync(Guid userId, int minRating, int maxRating, int page, int pageSize);
+
     public Task<IEnumerable<ProductReview>> GetReviewsByDateAsync(DateTime date, int page, int pageSize);
 }
